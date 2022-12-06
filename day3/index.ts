b4abac1e6af27024f78c231578fc9ec1f435c6b6
Samat <fatdrfrog@gmail.com>
2022-12-06 16:51:06 +0600

    day3 challenge

---
+++ day3/index.ts
@@ -0,0 +1,23 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const arrayOfInputs = input.split("\r\n");
+
+let sum = 0;
+for (let i = 0; i < arrayOfInputs.length; i += 3) {
+  let group = [];
+
+  for (let j = i; j < i + 3; j++) {
+    group.push(arrayOfInputs[j]);
+  }
+
+  const filteredChar = group[0]
+    .split("")
+    .filter((el) => group[1].includes(el) && group[2].includes(el))[0]
+    .charCodeAt(0);
+
+  //A = 65 , a= 97
+  const number =
+    filteredChar < 97 ? filteredChar - 65 + 27 : filteredChar - 97 + 1;
+  sum += number;
+}
+console.log(sum);
